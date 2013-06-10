7689cd49f82a6c2c4e7b671d17f488971b40db44
Jonathan Grant <jonathan.grant@redcross.org>
2013-06-10 14:22:02 -0400

    Initial

---
+++ GGC100th/GGCscript.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	/*$('.usingsite').click(function(){
+		$('.usingsite').hide();
+	});*/
+	$('.themephrase').mouseenter(function(){
+		$('.themephrase').css('color', '#99FF00');
+	}).mouseleave(function(){
+		$('.themephrase').css('color', '#330066');
+	});
+});
